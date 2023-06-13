1010a92beb3715cb1f4ba73a8782c58a214908d9
mingyu <mamingyu97@163.com>
2023-06-13 11:16:21 +0800

    NSQueueingLogicPriRL localization, wait still need to change

--- model/p4-model.h
+++ model/p4-model.h
@@ -35,6 +35,7 @@
 #include "ns3/delay-jitter-estimation.h"
 #include "ns3/event-id.h"
 #include "ns3/nstime.h"
+#include "ns3/simulator.h"
 #include <bm/bm_sim/queue.h>
 #include <bm/bm_sim/queueing.h>
 #include <bm/bm_sim/packet.h>
@@ -74,6 +75,357 @@ using bm::p4object_id_t;
 
 namespace ns3 {
 class P4NetDevice;
+
+
+//! This class is slightly more advanced than QueueingLogicRL. The difference
+//! between the 2 is that this one offers the ability to set several priority
+//! queues for each logical queue. Priority queues are numbered from `0` to
+//! `nb_priorities` (see NSQueueingLogicPriRL::NSQueueingLogicPriRL()). Priority `0`
+//! is the highest priority queue. Each priority queue can have its own rate and
+//! its own capacity. Queues will be served in order of priority, until their
+//! respective maximum rate is reached. If no maximum rate is set, queues with a
+//! high priority can starve lower-priority queues. For example, if the queue
+//! with priority `0` always contains at least one element, the other queues
+//! will never be served.
+//! As for QueueingLogicRL, the write behavior (push_front()) is not blocking:
+//! once a logical queue is full, subsequent incoming elements will be dropped
+//! until the queue starts draining again.
+//! Look at the documentation for QueueingLogic for more information about the
+//! template parameters (they are the same).
+template <typename T, typename FMap>
+class NSQueueingLogicPriRL {
+  using MutexType = std::mutex;
+  using LockType = std::unique_lock<MutexType>;
+
+ public:
+  //! See QueueingLogic::QueueingLogicRL() for an introduction. The difference
+  //! here is that each logical queue can receive several priority queues (as
+  //! determined by \p nb_priorities, which is set to `2` by default). Each of
+  //! these priority queues will initially be able to hold \p capacity
+  //! elements. The capacity of each priority queue can be changed later by
+  //! using set_capacity(size_t queue_id, size_t priority, size_t c).
+  NSQueueingLogicPriRL(size_t nb_workers, size_t capacity,
+                     FMap map_to_worker, size_t nb_priorities = 2)
+      : nb_workers(nb_workers),
+        capacity(capacity),
+        workers_info(nb_workers),
+        map_to_worker(std::move(map_to_worker)),
+        nb_priorities(nb_priorities) { }
+
+  //! If priority queue \p priority of logical queue \p queue_id is full, the
+  //! function will return `0` immediately. Otherwise, \p item will be copied to
+  //! the queue and the function will return `1`. If \p queue_id or \p priority
+  //! are incorrect, an exception of type std::out_of_range will be thrown (same
+  //! if the FMap object provided to the constructor does not behave correctly).
+  int push_front(size_t queue_id, size_t priority, const T &item) {
+    size_t worker_id = map_to_worker(queue_id);
+    LockType lock(mutex);
+    auto &q_info = get_queue(queue_id);
+    auto &w_info = workers_info.at(worker_id);
+    auto &q_info_pri = q_info.at(priority);
+    if (q_info_pri.size >= q_info_pri.capacity) return 0;
+    q_info_pri.last_sent = get_next_tp(q_info_pri);
+    w_info.queues[priority].emplace(
+        item, queue_id, q_info_pri.last_sent, w_info.wrapping_counter++);
+    q_info_pri.size++;
+    q_info.size++;
+    w_info.size++;
+    w_info.q_not_empty.notify_one();
+    return 1;
+  }
+
+  int push_front(size_t queue_id, const T &item) {
+    return push_front(queue_id, 0, item);
+  }
+
+  //! Same as push_front(size_t queue_id, size_t priority, const T &item), but
+  //! \p item is moved instead of copied.
+  int push_front(size_t queue_id, size_t priority, T &&item) {
+    size_t worker_id = map_to_worker(queue_id);
+    LockType lock(mutex);
+    auto &q_info = get_queue(queue_id);
+    auto &w_info = workers_info.at(worker_id);
+    auto &q_info_pri = q_info.at(priority);
+    if (q_info_pri.size >= q_info_pri.capacity) return 0;
+    q_info_pri.last_sent = get_next_tp(q_info_pri);
+    w_info.queues[priority].emplace(
+        std::move(item),
+        queue_id,
+        q_info_pri.last_sent,
+        w_info.wrapping_counter++);
+    q_info_pri.size++;
+    q_info.size++;
+    w_info.size++;
+    w_info.q_not_empty.notify_one();
+    return 1;
+  }
+
+  int push_front(size_t queue_id, T &&item) {
+    return push_front(queue_id, 0, std::move(item));
+  }
+
+  //! Retrieves an element for the worker thread indentified by \p worker_id and
+  //! moves it to \p pItem. The id of the logical queue which contained this
+  //! element is copied to \p queue_id and the priority value of the served
+  //! queue is copied to \p priority.
+  //! Elements are retrieved according to the priority queue they are in
+  //! (highest priorities, i.e. lowest priority values, are served first). Once
+  //! a given priority queue reaches its maximum rate, the next queue is served.
+  //! If no elements are available (either the queues are empty or they have
+  //! exceeded their rate already), the function will block.
+  void pop_back(size_t worker_id, size_t *queue_id, size_t *priority,
+                T *pItem) {
+    LockType lock(mutex);
+    auto &w_info = workers_info.at(worker_id);
+    MyQ *queue = nullptr;
+    size_t pri;
+    while (true) {
+      if (w_info.size == 0) {
+		// ns3::Simulator::Schedule
+        w_info.q_not_empty.wait(lock);
+      } else {
+        Time now = Simulator::Now();
+        Time next = now + Seconds (10); // set 10s as the max interval for one packet process.
+        for (pri = 0; pri < nb_priorities; pri++) {
+          auto &q = w_info.queues[pri];
+          if (q.size() == 0) continue;
+          if (q.top().send <= now) {
+            queue = &q;
+            break;
+          }
+          next = (next < q.top().send) ? next : q.top().send;
+        }
+        if (queue) break;
+        w_info.q_not_empty.wait_until(lock, next);
+      }
+    }
+    *queue_id = queue->top().queue_id;
+    *priority = pri;
+    // TODO(antonin): improve / document this
+    // http://stackoverflow.com/questions/20149471/move-out-element-of-std-priority-queue-in-c11
+    *pItem = std::move(const_cast<QE &>(queue->top()).e);
+    queue->pop();
+    auto &q_info = get_queue_or_throw(*queue_id);
+    auto &q_info_pri = q_info.at(*priority);
+    q_info_pri.size--;
+    q_info.size--;
+    w_info.size--;
+  }
+
+  //! Same as
+  //! pop_back(size_t worker_id, size_t *queue_id, size_t *priority, T *pItem),
+  //! but the priority of the popped element is discarded.
+  void pop_back(size_t worker_id, size_t *queue_id, T *pItem) {
+    size_t priority;
+    return pop_back(worker_id, queue_id, &priority, pItem);
+  }
+
+  //! @copydoc QueueingLogic::size
+  //! The occupancies of all the priority queues for this logical queue are
+  //! added.
+  size_t size(size_t queue_id) const {
+    LockType lock(mutex);
+    auto it = queues_info.find(queue_id);
+    if (it == queues_info.end()) return 0;
+    auto &q_info = it->second;
+    return q_info.size;
+  }
+
+  //! Get the occupancy of priority queue \p priority for logical queue with id
+  //! \p queue_id.
+  size_t size(size_t queue_id, size_t priority) const {
+    LockType lock(mutex);
+    auto it = queues_info.find(queue_id);
+    if (it == queues_info.end()) return 0;
+    auto &q_info = it->second;
+    auto &q_info_pri = q_info.at(priority);
+    return q_info_pri.size;
+  }
+
+  //! Set the capacity of all the priority queues for logical queue \p queue_id
+  //! to \p c elements.
+  void set_capacity(size_t queue_id, size_t c) {
+    LockType lock(mutex);
+    for_each_q(queue_id, SetCapacityFn(c));
+  }
+
+  //! Set the capacity of priority queue \p priority for logical queue \p
+  //! queue_id to \p c elements.
+  void set_capacity(size_t queue_id, size_t priority, size_t c) {
+    LockType lock(mutex);
+    for_one_q(queue_id, priority, SetCapacityFn(c));
+  }
+
+  //! Set the capacity of all the priority queues of all logical queues to \p c
+  //! elements.
+  void set_capacity_for_all(size_t c) {
+    LockType lock(mutex);
+    for (auto &p : queues_info) for_each_q(p.first, SetCapacityFn(c));
+    capacity = c;
+  }
+
+  //! Set the maximum rate of all the priority queues for logical queue \p
+  //! queue_id to \p pps. \p pps is expressed in "number of elements per
+  //! second". Until this function is called, there will be no rate limit for
+  //! the queue. The same behavior (no rate limit) can be achieved by calling
+  //! this method with a rate of 0.
+  void set_rate(size_t queue_id, uint64_t pps) {
+    LockType lock(mutex);
+    for_each_q(queue_id, SetRateFn(pps));
+  }
+
+  //! Same as set_rate(size_t queue_id, uint64_t pps) but only applies to the
+  //! given priority queue.
+  void set_rate(size_t queue_id, size_t priority, uint64_t pps) {
+    LockType lock(mutex);
+    for_one_q(queue_id, priority, SetRateFn(pps));
+  }
+
+  //! Set the rate of all the priority queues of all logical queues to \p pps.
+  void set_rate_for_all(uint64_t pps) {
+    LockType lock(mutex);
+    for (auto &p : queues_info) for_each_q(p.first, SetRateFn(pps));
+    queue_rate_pps = pps;
+  }
+
+  //! Deleted copy constructor
+  NSQueueingLogicPriRL(const NSQueueingLogicPriRL &) = delete;
+  //! Deleted copy assignment operator
+  NSQueueingLogicPriRL &operator =(const NSQueueingLogicPriRL &) = delete;
+
+  //! Deleted move constructor
+  NSQueueingLogicPriRL(NSQueueingLogicPriRL &&) = delete;
+  //! Deleted move assignment operator
+  NSQueueingLogicPriRL &&operator =(NSQueueingLogicPriRL &&) = delete;
+
+ private:
+ 
+  static constexpr Time rate_to_time(uint64_t pps) {
+    // 计算中间的时间间隔
+    return (pps == 0) ?
+        Seconds (0) : Seconds (static_cast<double>(1. / static_cast<double>(pps)));
+  }
+
+  struct QE {
+    QE(T e, size_t queue_id, const Time &send, size_t id)
+        : e(std::move(e)), queue_id(queue_id), send(send), id(id) { }
+
+    T e;
+    size_t queue_id;
+    Time send;
+    size_t id;
+  };
+
+  struct QEComp {
+    bool operator()(const QE &lhs, const QE &rhs) const {
+      return (lhs.send == rhs.send) ? lhs.id > rhs.id : lhs.send > rhs.send;
+    }
+  };
+
+  using MyQ = std::priority_queue<QE, std::deque<QE>, QEComp>;
+
+  struct QueueInfoPri {
+    QueueInfoPri(size_t capacity, uint64_t queue_rate_pps)
+        : capacity(capacity),
+          queue_rate_pps(queue_rate_pps),
+          pkt_delay_time(rate_to_time(queue_rate_pps)),
+          last_sent(Simulator::Now()) { }
+
+    size_t size{0};
+    size_t capacity;
+    uint64_t queue_rate_pps;
+    Time pkt_delay_time;
+    Time last_sent;
+  };
+
+  struct QueueInfo : public std::vector<QueueInfoPri> {
+    QueueInfo(size_t capacity, uint64_t queue_rate_pps, size_t nb_priorities)
+        : std::vector<QueueInfoPri>(
+              nb_priorities, QueueInfoPri(capacity, queue_rate_pps)) { }
+
+    size_t size{0};
+  };
+
+  struct WorkerInfo {
+    mutable std::condition_variable q_not_empty{};
+    size_t size{0};
+    std::array<MyQ, 32> queues;
+    size_t wrapping_counter{0};
+  };
+
+  QueueInfo &get_queue(size_t queue_id) {
+    auto it = queues_info.find(queue_id);
+    if (it != queues_info.end()) return it->second;
+    auto p = queues_info.emplace(
+        queue_id, QueueInfo(capacity, queue_rate_pps, nb_priorities));
+    return p.first->second;
+  }
+
+  const QueueInfo &get_queue_or_throw(size_t queue_id) const {
+    return queues_info.at(queue_id);
+  }
+
+  QueueInfo &get_queue_or_throw(size_t queue_id) {
+    return queues_info.at(queue_id);
+  }
+
+  Time get_next_tp(const  QueueInfoPri &q_info_pri) {
+    // 计算出下一步应该发送的时间
+    return (Simulator::Now() > q_info_pri.last_sent + q_info_pri.pkt_delay_time) ? 
+            Simulator::Now() : q_info_pri.last_sent + q_info_pri.pkt_delay_time;
+  }
+
+  template <typename Function>
+  Function for_each_q(size_t queue_id, Function fn) {
+    auto &q_info = get_queue(queue_id);
+    for (auto &q_info_pri : q_info) fn(q_info_pri);
+    return fn;
+  }
+
+  template <typename Function>
+  Function for_one_q(size_t queue_id, size_t priority, Function fn) {
+    auto &q_info = get_queue(queue_id);
+    auto &q_info_pri = q_info.at(priority);
+    fn(q_info_pri);
+    return fn;
+  }
+
+  struct SetCapacityFn {
+    explicit SetCapacityFn(size_t c)
+        : c(c) { }
+
+    void operator ()(QueueInfoPri &info) const {  // NOLINT(runtime/references)
+      info.capacity = c;
+    }
+
+    size_t c;
+  };
+
+  struct SetRateFn {
+    explicit SetRateFn(uint64_t pps)
+        : pps(pps) {
+      pkt_delay_time = rate_to_time(pps);
+    }
+
+    void operator ()(QueueInfoPri &info) const {  // NOLINT(runtime/references)
+      info.queue_rate_pps = pps;
+      info.pkt_delay_time = pkt_delay_time;
+    }
+
+    uint64_t pps;
+    Time pkt_delay_time;
+  };
+
+  mutable MutexType mutex;
+  size_t nb_workers;
+  size_t capacity;  // default capacity
+  uint64_t queue_rate_pps{0};  // default rate
+  std::unordered_map<size_t, QueueInfo> queues_info{};
+  std::vector<WorkerInfo> workers_info{};
+  std::vector<MyQ> queues{};
+  FMap map_to_worker;
+  size_t nb_priorities;
+};
 
 /**
 * \brief A P4 Pipeline Implementation to be wrapped in P4 Device
@@ -193,6 +545,7 @@ class P4Model : public Switch {
 		class MirroringSessions;
 
 		class InputBuffer;
+		// template <typename T, typename FMap> class NSQueueingLogicPriRL;
 
 		enum PktInstanceType {
 			PKT_INSTANCE_TYPE_NORMAL,
@@ -245,8 +598,7 @@ class P4Model : public Switch {
 		// for these queues, the write operation is non-blocking and we drop the
 		// packet if the queue is full
 		size_t nb_queues_per_port;
-		bm::QueueingLogicPriRL<std::unique_ptr<bm::Packet>, EgressThreadMapper>
-		egress_buffers;
+		NSQueueingLogicPriRL<std::unique_ptr<bm::Packet>, EgressThreadMapper> egress_buffers;
 		bm::Queue<std::unique_ptr<bm::Packet> > output_buffer;
 		TransmitFn my_transmit_fn;
 		std::shared_ptr<McSimplePreLAG> pre;
